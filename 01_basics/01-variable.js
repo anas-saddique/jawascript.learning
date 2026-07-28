9bef19312e498786d5362b9ee51cbaaf9971a1ae
anas <anassaddique50@gmail.com>
2026-07-28 04:18:25 -0700

    first commit

---
+++ 01_basics/01-variable.js
@@ -0,0 +1,24 @@
+ const studentId = 12345679;
+ let studentName = "anassiddique";
+ let studentAge = "20"
+ let studentCity = "bhawalpur";
+ let isStudent = "yes";
+
+
+//  accountId = 987654321;
+
+// accountEmail = "anassiddique@example.com"
+// accountPassword = "54321"
+// accountCity = "lahore"
+
+
+//  console.log(accountId);
+
+ console.table([
+ studentId,
+  studentName,
+  studentAge,
+  studentCity,
+  isStudent,
+    ]  
+)
---
+++ 01_basics/02_variable.js
@@ -0,0 +1,8 @@
+    let score = "100";
+
+    console.log( typeof score)
+console.log( typeof (score))
+
+let valueInnumber = Number(score);
+console.log( typeof (valueInnumber))
+console.log(valueInnumber)
