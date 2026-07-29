96f32bfd7eaa11203101f27c7837b7072ee29b7a
anas <anassaddique50@gmail.com>
2026-07-29 08:25:31 -0700

    learning

---
+++ 01_basics/03_converstaion.js
@@ -0,0 +1,11 @@
+let str1 = "Hello"
+let str2 = "Anassiddique"
+
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log( "1" + 2);
+console.log( 1 + "2");
+console.log( 1 + 2 + "3");
+console.log( "1" + 2 + 3);
