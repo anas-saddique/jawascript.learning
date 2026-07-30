50466da80bf340be2c06d1e5223b17016e0ed1c3
anas <anassaddique50@gmail.com>
2026-07-30 03:15:09 -0700

    work in datatype.summary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,20 @@
+let myname = "Anas"
+
+let anothername = myname 
+anothername = "Ali"
+
+
+console.log(myname)
+console.log(anothername)
+
+let userone = {
+    name: "Anas",
+    age: 20
+}
+
+let usertwo = userone
+usertwo.name = "anassiddique"
+
+console.log(userone.name)
+console.log(usertwo.name)
+
