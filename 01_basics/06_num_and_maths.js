488da6e9dee287e219b6a5af86a7d004dc2fe294
anas <anassaddique50@gmail.com>
2026-07-31 02:58:22 -0700

    work on math in js

--- 01_basics/06_num_and_maths.js
+++ 01_basics/06_num_and_maths.js
@@ -12,3 +12,27 @@ console.log(otherNumber.toPrecision(3));
 
 const hundred = 100000;
 console.log(hundred.toLocaleString("en-pk"));
+
+
+//==============maths===============//
+
+console.log(Math)
+console.log(Math.abs(4))
+console.log(Math.round(4.5))
+console.log(Math.ceil(4.5))
+console.log(Math.min(4,5,6,7,8,2,7))
+console.log(Math.max(4,5,6,7,8,2,7))
+
+
+console.log(Math.random())
+console.log((Math.random()*10)+ 1)
+console.log((Math.random()*10)+ 1)
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max -min + 1)) + min)
+
+
+
