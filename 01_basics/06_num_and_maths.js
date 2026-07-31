da697ac21ebf587da8089b23d288777b979c8379
anas <anassaddique50@gmail.com>
2026-07-31 02:22:16 -0700

    learning-maths & num

---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,14 @@
+const score = 400;
+console.log(score); 
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.456;
+console.log(otherNumber.toPrecision(3));
+
+const hundred = 100000;
+console.log(hundred.toLocaleString("en-pk"));
