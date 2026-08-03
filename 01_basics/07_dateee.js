1c59c78dd268abc4a8f6dba185cef4dc44ab2379
anas <anassaddique50@gmail.com>
2026-08-03 04:24:10 -0700

    WORK ON MOTHS,TIME

--- 01_basics/07_dateee.js
+++ 01_basics/07_dateee.js
@@ -1,2 +1,29 @@
-const myDate = new Date ()
+const myDate = new Date();
+
 console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log( typeof myDate);
+
+
+// let myCreatedDate = new Date(2023, 0, 23,5 ,3)
+// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString())
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp)
+console.log(myCreatedDate.getTime())
+console.log(Date.now ())
+  
+let newDate = new Date()
+console.log(newDate)
+console.log(newDate.getMonth())
+// console.log(newDate.getmyTimeStamp())
+console.log(newDate.getTime());
+
+const newDate = new Date();
+newDate.localeString('default', { weekday: "long"})
+
+
