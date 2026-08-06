33dbca48c3ad6fefbf5c4a10f9a481e1213a49dc
anas <anassaddique50@gmail.com>
2026-08-06 08:32:27 -0700

    work

--- 02-basics/04_object.js
+++ 02-basics/04_object.js
@@ -23,7 +23,8 @@ const regularUser = {
 
 const obj1 = {1: "a", 2: "b"};
 const obj2 = {3: "c", 4: "d"};
+const obj4 = {5: "e", 4: "f"};
 
-const obj3 = { obj1, obj2 };
+const obj3 = Object.assign({}, obj1, obj2, obj4) ;
 
 console.log(obj3);
