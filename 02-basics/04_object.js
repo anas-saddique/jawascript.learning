dcfd26a268c71854b4fb2dc674b80b70e0dcd180
anas <anassaddique50@gmail.com>
2026-08-06 03:45:04 -0700

    work on line 12 regular user

--- 02-basics/04_object.js
+++ 02-basics/04_object.js
@@ -1,10 +1,21 @@
 // const tinderUser = new Object()
 
 const tinderUser = {}
+
 tinderUser.id = "123abc";
 tinderUser.name = "Anas";
 tinderUser.isLoggedIn = false;
 
 
-console.log(tinderUser);
+// console.log(tinderUser);
 
+const regularUser = {
+    email: "regular@example.com",
+    fullname: {
+        userFullname: {
+            firstName: "Anas",
+            lastName: "siddique"
+        }
+    }
+};
+console.log(regularUser.fullname);
