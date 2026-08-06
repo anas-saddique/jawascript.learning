be707471a8ececaeb7b0b79f1ec119d043c81f00
anas <anassaddique50@gmail.com>
2026-08-06 02:44:46 -0700

    work on object- symbole

---
+++ 02-basics/03_object.js
@@ -0,0 +1,18 @@
+
+const mySym=Symbol("key1");
+
+
+const Jsuser = {
+ "full name": "Anas",
+  age: 20,
+  mySym: "key1",
+  email: "anas@example.com",
+  location: "Pakistan",
+  isActive: false,
+  lastLogin: "more than 30 days ago"
+};
+
+console.log(Jsuser.email);
+console.log(Jsuser["email"]);
+console.log(Jsuser["full name"]);
+console.log(Jsuser[mySym]);
