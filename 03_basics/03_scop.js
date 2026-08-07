c1e3ac993b506fedf871a1fb672eeb44d7bde339
anas <anassaddique50@gmail.com>
2026-08-07 05:24:07 -0700

    working on scope

---
+++ 03_basics/03_scop.js
@@ -0,0 +1,41 @@
+function one(){
+    const username = "Anas"
+
+    function two(){
+        const website = "siddique.versal"
+        console.log(username)
+    }
+    // console.log(website)
+    two()
+}
+// one()
+if (true){ 
+    const  username="Anas"
+    if(username === "Anas"){
+        const website ="siddique.versal"
+        // console.log(username+website);
+    }
+    // console.log(website)
+}
+// console.log(username)
+
+
+
+
+// =========================intersting question==============================
+
+console.log(addone (5))
+
+function addone (num) {
+    return num + 1
+}
+
+
+
+// addtwo(5)
+
+// const addtwo = function(num){
+//     return num + 2
+// }
+
+
