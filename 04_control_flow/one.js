ff7cd20993f05a498d268b20fb97df41bb039ab4
anas <anassaddique50@gmail.com>
2026-08-08 15:17:29 -0700

    work on control flow : user logged in and many things

---
+++ 04_control_flow/one.js
@@ -0,0 +1,47 @@
+// const isUserLoggedIn =true
+// const temprature = 41
+
+// if (temprature === 41){
+//     console.log("less than 50")
+// }
+// else{
+//     console.log("temprature grater than 50")
+// }
+
+// console.log("excute")
+
+
+// const score = 200
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`user power : ${power}`)
+// }
+// console.log(`user power : ${power}`)
+
+
+
+// const balance = 1000
+
+// if (balance < 500){
+//     console.log("less than 500")
+// } else if (balance < 750){
+//     console.log("less than 750")
+// } else if(balance < 950){
+//     console.log("less than 950")
+// } else {
+//     console.log("grater than 1200")
+// } 
+
+
+const userLoggedIn = true
+const debitCard = true
+const userLoggedInFromEmail =true
+const userLoggedInFromGoogle = false
+if (userLoggedIn && debitCard && 2==2){
+    console.log("Aloow to Buy")
+}
+
+if(userLoggedInFromGoogle || userLoggedInFromEmail){
+    console.log("usesr logged in")
+}
