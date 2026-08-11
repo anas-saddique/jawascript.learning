fa87b3cccb073561a8ffec52130a1dffc5de1fa3
anas <anassaddique50@gmail.com>
2026-08-11 02:23:30 -0700

    work on forin loops

---
+++ 05_loops/forin.js
@@ -0,0 +1,14 @@
+const myObject ={
+     js : 'jawascript',
+     cpp : 'c++',
+     rb : 'ruby'
+}  
+for (const key in myObject) {
+console.log(`${key} shoutcut is for ${myObject[key]} `)
+}
+
+const programing = ["js","html","css"]
+for (const key in programing) {
+  console.log(programing[key])   
+}
+ 
