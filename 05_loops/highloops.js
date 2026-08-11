563fc90e7b2ab236e8a44fbe28d61ba8c28613f6
anas <anassaddique50@gmail.com>
2026-08-11 02:23:13 -0700

    work on fornof loops

---
+++ 05_loops/highloops.js
@@ -0,0 +1,33 @@
+// const arr =[1,2,3,4,5]
+
+// for (const num of arr) {
+// // console.log(num)    
+// }
+
+// const greetings = "HELLO WORLD!"
+// for (const greet of greetings) {
+//     // console.log(`Each char is ${greet}`)
+// }
+
+
+// const map = new Map()
+
+// map.set('pk'  , 'Pakistan')
+// map.set('fr'  , 'france')
+// map.set('uk'  , 'united kingdom')
+
+// // console.log(map)
+
+// for (const [key, value] of map){
+//         // console.log(key, ':-', value)
+// }
+
+// const myObject = (
+//     game1: 'NFS'
+//     game1: 'taken'
+// )
+
+
+// for (const [key,value] of myObject){
+//     console.log(key, ':-', value)
+// }
