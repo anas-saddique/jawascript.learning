a578d564217b37129ce0a23c3df60f08d0dc5f94
anas <anassaddique50@gmail.com>
2026-08-11 07:25:33 -0700

    work on loop

---
+++ 05_loops/part.js
@@ -0,0 +1,27 @@
+const coding  =["js","html","css"]
+
+
+const value = coding.forEach((item)=>{
+    console.log(item)
+})
+
+console.log(value)
+
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+ 
+
+// const newNums = myNums.filter((num) => 4)
+// console.log(newNums)
+// const newNums = myNums.filter((num) => {
+//       return  num > 4
+// })
+
+const nemNums = []
+
+myNums.forEach( (num) => {
+    if (num > 4) {
+        nemNums.push(num)
+    }
+})
+console.log(nemNums);
